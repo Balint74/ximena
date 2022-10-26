204614db13f4413632c8194c9a714fce9966642c
Fehér Bálint <feherbalint74@gmail.com>
2022-10-26 15:43:23 +0200

    added pics

--- components/Showcase.js
+++ components/Showcase.js
@@ -10,6 +10,81 @@ const Showcase = () => {
       </h1>
 
       <div className="grid sm:grid-cols-2 md:grid-cols-3 sm:gap-x-20 gap-y-5 sm:gap-y-20 px-10 pb-14 shadow-lg">
+      <motion.div
+          whileHover={{
+            scale: 1.3,
+            transition: { duration: 0.7 },
+          }}
+          className="hover:z-40 z-10 relative mx-auto flex h-64 w-64 lg:h-72 lg:w-72 items-center justify-center shadow-2xl"
+        >
+          <Image
+            src={"/assets/m7.jpg"}
+            alt="Mandala"
+            width={500}
+            height={500}
+            className="rounded-xl shadow-2xl"
+          ></Image>
+        </motion.div>
+        <motion.div
+          whileHover={{
+            scale: 1.3,
+            transition: { duration: 0.7 },
+          }}
+          className="hover:z-40 z-10 relative mx-auto flex h-64 w-64 lg:h-72 lg:w-72 items-center justify-center shadow-2xl"
+        >
+          <Image
+            src={"/assets/m8.jpg"}
+            alt="Mandala"
+            width={500}
+            height={500}
+            className="rounded-xl shadow-2xl"
+          ></Image>
+        </motion.div>
+        <motion.div
+          whileHover={{
+            scale: 1.3,
+            transition: { duration: 0.7 },
+          }}
+          className="hover:z-40 z-10 relative mx-auto flex h-64 w-64 lg:h-72 lg:w-72 items-center justify-center shadow-2xl"
+        >
+          <Image
+            src={"/assets/m9.jpg"}
+            alt="Mandala"
+            width={500}
+            height={500}
+            className="rounded-xl shadow-2xl"
+          ></Image>
+        </motion.div>
+        <motion.div
+          whileHover={{
+            scale: 1.3,
+            transition: { duration: 0.7 },
+          }}
+          className="hover:z-40 z-10 relative mx-auto flex h-64 w-64 lg:h-72 lg:w-72 items-center justify-center shadow-2xl"
+        >
+          <Image
+            src={"/assets/m10.jpg"}
+            alt="Mandala"
+            width={500}
+            height={500}
+            className="rounded-xl shadow-2xl"
+          ></Image>
+        </motion.div>
+        <motion.div
+          whileHover={{
+            scale: 1.3,
+            transition: { duration: 0.7 },
+          }}
+          className="hover:z-40 z-10 relative mx-auto flex h-64 w-64 lg:h-72 lg:w-72 items-center justify-center shadow-2xl"
+        >
+          <Image
+            src={"/assets/m11.jpg"}
+            alt="Mandala"
+            width={500}
+            height={500}
+            className="rounded-xl shadow-2xl"
+          ></Image>
+        </motion.div>
         <motion.div
           whileHover={{
             scale: 1.3,
@@ -20,7 +95,8 @@ const Showcase = () => {
           <Image
             src={"/assets/m1.jpg"}
             alt="Mandala"
-            layout="fill"
+            width={500}
+            height={500}
             className="rounded-xl shadow-2xl"
           ></Image>
         </motion.div>
@@ -34,7 +110,8 @@ const Showcase = () => {
           <Image
             src={"/assets/m2.jpg"}
             alt="Mandala"
-            layout="fill"
+            width={500}
+            height={500}
             className="rounded-xl shadow-2xl"
           ></Image>
         </motion.div>
@@ -48,7 +125,8 @@ const Showcase = () => {
           <Image
             src={"/assets/m3.jpg"}
             alt="Mandala"
-            layout="fill"
+            width={500}
+            height={500}
             className="rounded-xl shadow-2xl"
           ></Image>
         </motion.div>
@@ -62,7 +140,8 @@ const Showcase = () => {
           <Image
             src={"/assets/m4.jpg"}
             alt="Mandala"
-            layout="fill"
+            width={500}
+            height={500}
             className="rounded-xl shadow-2xl"
           ></Image>
         </motion.div>
@@ -76,7 +155,8 @@ const Showcase = () => {
           <Image
             src={"/assets/m5.jpg"}
             alt="Mandala"
-            layout="fill"
+            width={500}
+            height={500}
             className="rounded-xl shadow-2xl"
           ></Image>
         </motion.div>
@@ -90,7 +170,8 @@ const Showcase = () => {
           <Image
             src={"/assets/m6.jpg"}
             alt="Mandala"
-            layout="fill"
+            width={500}
+            height={500}
             className="rounded-xl shadow-2xl"
           ></Image>
         </motion.div>
